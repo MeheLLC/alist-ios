b0e54a5e77527d71a80703f74d0885ad99936f64
Jamone Kelly <xg.monie089@gmail.com>
2016-02-19 01:13:01 -0500

    Initial Commit

---
+++ A List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  A List
+//
+//  Created by Jamone Alexander Kelly on 2/19/16.
+//  Copyright © 2016 A List. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
