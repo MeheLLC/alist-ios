c03bc44d8074bd4f83cfe6007f4c445586c6d724
Jamone Kelly <xg.monie089@gmail.com>
2016-02-21 01:08:24 -0500

    Add barebones sign up view controller

---
+++ A List/SignupWithEmail.swift
@@ -0,0 +1,24 @@
+//
+//  SignupWithEmail.swift
+//  A List
+//
+//  Created by Jamone Alexander Kelly on 2/21/16.
+//  Copyright © 2016 A List. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class SignupWithEmail: BaseViewController {
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = Localizable("Sign up")
+    }
+}
